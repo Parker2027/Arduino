c44e79fe84c62bfa2cd8a919392a7cf83572288b
Parker2027 <prbrayjo@uwaterloo.ca>
2023-05-16 16:37:04 -0400

    asdfgh

--- sketch_may16a/sketch_may16a.ino
+++ sketch_may16a/sketch_may16a.ino
@@ -1,3 +1,10 @@
+#include <Boards.h>
+#include <Firmata.h>
+#include <FirmataConstants.h>
+#include <FirmataDefines.h>
+#include <FirmataMarshaller.h>
+#include <FirmataParser.h>
+
 void setup() {
   // put your setup code here, to run once:
 
